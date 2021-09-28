73672d9507abced1f64bb2815e955a3864b9333c
estebanmendez97 <estebanm289@gmail.com>
2021-09-27 21:20:03 -0500

    fixed gap in header buttons working

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-let btnGet = document.querySelector('#btn-get');
-let selGet = document.querySelector('#sel-get');
-let cart = document.querySelector('#cart');
+let btnGet = document.querySelector("#btn-get");
+let btnOGet = document.querySelector("btn-o-get");
+let btnTGet = document.querySelector("#btn-t-get");
+let btnBoGet = document.querySelector("#btn-bo-get");
 
-btnGet.addEventListener('click', () =>{
-    cart.innerText = selGet.value;
-})
+let selGet = document.querySelector("#sel-get");
+let selOGet = document.querySelector("#sel-o-get");
+let selTGet = document.querySelector("#sel-t-get");
+let selBoGet = document.querySelector("#sel-bo-get");
+
+let cart = document.querySelector("#cart");
+
+btnGet.addEventListener("click", () => {
+  cart.innerText = selGet.value;
+});
+
+btnOGet.addEventListener("click", () => {
+  cart.innerText = selOGet.value;
+});
+
+btnTGet.addEventListener("click", () => {
+  cart.innerText = selTGet.value;
+});
+
+btnBoGet.addEventListener("click", () => {
+  cart.innerText = selBoGet.value;
+});
