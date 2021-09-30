1bd98c700a40391caf0a9224171acf5a4c2b3bbe
estebanmendez97 <estebanm289@gmail.com>
2021-09-29 21:18:00 -0500

    fixed id for the buy started function for button

--- main.js
+++ main.js
@@ -1,40 +1,14 @@
 let btnGet = document.querySelector("#btn-get");
-let btnOnGet = document.querySelector("#btn-on-get");
-let btnTGet = document.querySelector("#btn-t-get");
-let btnBoGet = document.querySelector("#btn-bo-get");
-let btnBtGet = document.querySelector("#btn-bt-get");
-let btnBtrGet = document.querySelector("#btn-btr-get");
 
-let selGet = document.querySelector("#sel-get");
-let selOnGet = document.querySelector("#sel-on-get");
-let selTGet = document.querySelector("#sel-t-get");
-let selBoGet = document.querySelector("#sel-bo-get");
-let selBtGet = document.querySelector("#sel-bt-get");
-let selBtrGet = document.querySelectorAll("#sel-btr-get");
+for(let i=0;i > btnGet.length; 1++) {
+btnGet[i].addEventistener("click", () => {
+  cartNumbers()
+})
+}
 
-let cart = document.querySelector("#cart");
+function cartNumbers() {
+   let productNumbers = localStorage.getItem('cartNumbers');
+   productNumbers = parseInt(productNumbers)
+   localStorage.setItem('cartNumber', 1)
 
-btnGet.addEventListener("click", () => {
-  cart.innerText = selGet.value;
-});
-
-btnOnGet.addEventListener("click", () => {
-cart.innerText = selOnGet.value;
-});
-
-btnTGet.addEventListener("click", () => {
-  cart.innerText = selTGet.value;
-});
-
-btnBoGet.addEventListener("click", () => {
-  cart.innerText = selBoGet.value;
-});
-
-btnBtGet.addEventListener("click", () => {
-  cart.innerText = selBtGet.value;
-
-});
-
-btnBtrGet.addEventListener("click", () => {
-  cart.innerText = selBtGet.value; 
-});
+}
