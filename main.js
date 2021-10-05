5dfed5f1d6e2ac801e8ec4f1bd386268a1fde3cd
estebanmendez97 <estebanm289@gmail.com>
2021-10-05 13:49:01 -0500

    buy buttons working and displaying the ammounts of items on cart

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-let btnGet = document.querySelector("#btn-get");
+let btnGet = document.querySelectorAll(".btn-get");
 
-for(let i=0;i > btnGet.length; 1++) {
-btnGet[i].addEventistener("click", () => {
-  cartNumbers()
-})
+for (let i = 0; i < btnGet.length; i++) {
+  btnGet[i].addEventListener("click", () => {
+    cartNumbers();
+  });
 }
 
 function cartNumbers() {
-   let productNumbers = localStorage.getItem('cartNumbers');
-   productNumbers = parseInt(productNumbers)
-   localStorage.setItem('cartNumber', 1)
+  let productNumbers = localStorage.getItem("cartNumbers");
 
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector('.cart span').textContent = productNumbers +1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector('.cart span').textContent = 1;
+  }
 }
+console.log("running");
